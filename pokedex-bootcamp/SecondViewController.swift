22be48c371ef4bab9d4d63b359c50e2d3a16f0ff
EnmanuelSandino <sandino.abc@gmail.com>
2023-04-18 15:02:46 -0700

    feat: added-second-view-controller

---
+++ pokedex-bootcamp/SecondViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SecondViewController.swift
+//  pokedex-bootcamp
+//
+//  Created by Bootcamp on 4/18/23.
+//
+
+import Foundation
--- pokedex-bootcamp/ViewController.swift
+++ pokedex-bootcamp/ViewController.swift
@@ -17,12 +17,24 @@ class ViewController: UIViewController {
         setUI()
     }
     
+    
     private func setUI() {
         welcomeTitle.font = UIFont.systemFont(ofSize: 34)
     }
+    
 
     @IBAction func didTapButton(_ sender: Any) {
         print("Button clicked")
     }
+    
+    /*func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            if let vc = storyboard?.instantiateViewController(identifier: "SecondViewController") as? SecondViewController {
+                vc.photoSecondVC = pokemons[indexPath.row].photo
+                vc.nameSecondVC = pokemons[indexPath.row].name
+                vc.descriptionSecondVC = pokemons[indexPath.row].longDescription
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+        }
+     */
 }
 
